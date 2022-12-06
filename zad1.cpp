ab2f3a2e544d1bd0d66ba923d90d69a50b3b65bf
keknuts <62103742+keknuts@users.noreply.github.com>
2022-12-07 00:37:07 +0800

    Update zad1.cpp

--- zad1.cpp
+++ zad1.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 
@@ -12,26 +5,14 @@ using namespace std;
 
 int main()
 {
-    int x;
-    int y;
-    int z;
-    int q;
-     int summa = 0;
+    int x = 11 + 0;
+    int y = x - 2;
+    int z = y * 3;
+    int q = z / 2;
      
-    cout << "Введите целое число: ";
-     cin >> x;
-     
-    cout << "Введите целое число еще раз: ";
-    cin >> y;
     
-    cout << "Введите целое число и еще раз: ";
-    cin >> z;
-    
-    cout << "Введите целое число, крайний раз )): ";
-    cin >> q;
-    
-summa = x + y + z + q;
-cout << "Сумма введенных вами чисел равна: " << summa << endl;
+
+cout<<x<<"\t"<<y<<"\n"<<z<<"\t"<<q<<"\n";
 
     return 0;
 }
