7e808151822beb6f227a91e7ab4f99da0c2ae136
keknuts <62103742+keknuts@users.noreply.github.com>
2022-12-08 17:47:43 +0800

    Add files via upload

---
+++ zad 2 (1).cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      for(int a=0 ; a<=100 ; a++ )
+      
+      
+        if(a%3 == 0)
+        
+        cout<<a<<"\t";
+    
+    cout<<"\n";
+}
+
