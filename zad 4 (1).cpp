73f1c0af7193959af5e57cb5a59f4ea0005776fa
keknuts <62103742+keknuts@users.noreply.github.com>
2022-12-11 00:22:28 +0800

    Add files via upload

---
+++ zad 4 (1).cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+	
+
+	
+	string num[10]={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	
+	
+
+		int a;
+		
+		cin>>a;
+		
+		cout<<num[a]<<"\n";
+		
+	
+	
+  	return 0;
+} 
+  	
